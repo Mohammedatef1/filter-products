adbe0bae1cb229b79515b4f745116692ee605660
Mohammed Atef <atefmohammed@std.mans.edu.eg>
2024-07-24 15:30:25 +0300

    add: products API

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,1 +1,21 @@
+import { db } from "@/db";
+import { NextApiRequest, NextApiResponse } from "next";
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "method not allowed" });
+  }
+
+  try {
+    const products = await db.query({
+      topK: 12,
+      vector: [0, 0, 0],
+      includeData: true,
+    });
+
+    return res.status(200).json(products);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: "Interval server error." });
+  }
+}
